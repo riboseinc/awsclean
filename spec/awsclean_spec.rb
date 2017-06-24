7fe3972613773024c28ece6235f2adcaebfdc11c
mksm <mksm@iddqd.su>
2017-06-24 10:13:58 -0300

    Add Travis CI config and badge
    - Requires Ruby >= 2.3
    - Travis builds on Ruby 2.3 and 2.4

--- spec/awsclean_spec.rb
+++ spec/awsclean_spec.rb
@@ -1,11 +1,4 @@
 require "spec_helper"
 
 RSpec.describe Awsclean do
-  it "has a version number" do
-    expect(Awsclean::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
